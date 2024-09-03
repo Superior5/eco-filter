4ec15e4be832bd9d1e84d7d8490ff1763cbbd108
Superior5 <superiorunknown5@gmail.com>
2024-09-04 01:57:44 +0300

    github pages

--- components/pages/main/section/3.vue
+++ components/pages/main/section/3.vue
@@ -1,5 +1,5 @@
 <template>
-   <section id="section-3" class="bg-[#EDEDED] h-screen pt-40px w-full">
+   <section id="section-3" class="bg-[#EDEDED] md:h-screen pt-40px pb-20px w-full">
       <div class="container font-mulish w-full h-full flex flex-col items-center">
          <h3 class="font-bold text-30px">Устройства</h3>
          <p class="my-40px text-center">На данный момент разработано две версии устройства</p>
@@ -7,19 +7,29 @@
          <div class="w-full relative font-bold text-24px max-w-1000px grid grid-cols-1">
             <div class="grid grid-cols-2">
                <div class="h-300px bg-[#798d9e] border-black"></div>
-               <div class="flex justify-center items-center">
+               <div class="flex bg-white justify-center items-center">
                   CenAir
                </div>
             </div>
             <div class="grid grid-cols-2">
-               <div class="flex justify-center items-center">
-                  CenAir+
+               <div class="flex flex-col gap-10px bg-white justify-center items-center">
+                   <p>CenAir+</p>
+                  <p class="text-center max-w-250px font-thin text-16px">Данная версия имеет датчик углекислого газа (СО2), возможность управления устройством с мобильного
+                     телефона</p>
                </div>
                <div class="h-300px bg-[#798d9e]"></div>
             </div>
             <div class="absolute top-[48.3%] left-[48.3%] w-20px h-20px rounded-[50%] bg-[#000] text-18px font-normal text-white flex justify-center items-center">+</div>
          </div>
+      <div class="container w-full max-w-1000px flex gap-30px md:(flex-row mt-0 items-end ) mt-20px flex-col items-normal justify-between">
+         <p class="max-w-500px">На данный момент устройство производится только по
+            индивидуальному заказу.</p>
+      <button @click="navigateTo('/#section-5')" class="bg-[#0caad4] md:mt-30px text-white font-bold px-30px py-15px">Отправить</button>
+
       </div>
+
+      </div>
+
    </section>
 </template>
 <style scoped>
